d230943a6f64b4c5d670c71deef86d786dd3e936
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-01-28 17:39:50 +0100

    don't redefine MIRYOKU_LAYER_BASE

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -17,10 +17,8 @@ XXX  XXX  XXX  XXX  XXX            K33  K34  K32                 K35  K37  K36  
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_ADV360
 
-#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPC),  U_LT(U_MEDIA, RET),U_LT(U_FUN, TAB),  U_LT(U_NUM, BSPC), U_LT(U_SYM, DEL),  U_NP,              U_NP
-
-#define MIRYOKU_LAYER_BASE MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH
