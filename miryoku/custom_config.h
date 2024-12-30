84eb523c8d8d86fe0b42513d6afbc0f915f008fe
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2024-12-30 19:00:03 +0100

    Add keymap changes from Kinesis 3.0 branch
    Kinesis has changed the keymap file:
    https://github.com/KinesisCorporation/Adv360-Pro-ZMK/blob/V3.0/UPGRADE.md
    
    This was causing wrong alignment of keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -36,7 +36,7 @@
 XXX  XXX  XXX  XXX  XXX  XXX  XXX                                               XXX  XXX  XXX  XXX  XXX  XXX  XXX \
 XXX  K00  K01  K02  K03  K04  XXX                                               XXX  K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14  XXX       K32  K32                 K35  K35       XXX  K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       XXX  XXX  K32  XXX       XXX  K35  XXX  XXX       K25  K26  K27  K28  K29  XXX \
+XXX  K20  K21  K22  K23  K24                 K32                 K35                 K25  K26  K27  K28  K29  XXX \
 XXX  XXX  XXX  XXX  K32            K33  K34  K32                 K35  K37  K36            K35  XXX  XXX  XXX  XXX
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_ADV360PRO
