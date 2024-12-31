19a4aafae45352c47ecf12a78d5a47449d81a9aa
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2024-12-31 14:00:36 +0100

    Rename adv360 to adv360pro (#62)
    Following the upstream Miryoku ZMK commit:
    
    https://github.com/manna-harbour/miryoku_zmk/commit/5971b6cf0903fe705cb9a3892e486a0d074a7e6f#diff-1f90ee3e9b2436305a61900cfc26c99113af61f7fb2135db297c0c68dae86112

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -27,7 +27,7 @@
 
 #define XXX &none
 
-#define MIRYOKU_LAYOUTMAPPING_ADV360( \
+#define MIRYOKU_LAYOUTMAPPING_ADV360PRO( \
      K00, K01, K02, K03, K04,                                                        K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                                                        K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                                                        K25, K26, K27, K28, K29, \
@@ -39,7 +39,7 @@ XXX  K10  K11  K12  K13  K14  XXX       K32  K32                 K35  K35       
 XXX  K20  K21  K22  K23  K24       XXX  XXX  K32  XXX       XXX  K35  XXX  XXX       K25  K26  K27  K28  K29  XXX \
 XXX  XXX  XXX  XXX  K32            K33  K34  K32                 K35  K37  K36            K35  XXX  XXX  XXX  XXX
 
-#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_ADV360
+#define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_ADV360PRO
 
 #define MIRYOKU_LAYER_BASE \
 &kp W,             &kp L,             &kp Y,                        &kp P,                                   &kp B,                      &kp Z,                    &kp F,                      &kp O,                    &kp U,             U_DQUOTES_SQUOTES,      \
